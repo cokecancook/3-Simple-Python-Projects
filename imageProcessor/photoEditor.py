bfd73739718c2bc427ad3d232cc852d3db131aea
Coke Stuyck <cokecancook@gmail.com>
2024-12-04 21:56:44 +0100

    Upload Image Processor

--- imageProcessor/photoEditor.py
+++ imageProcessor/photoEditor.py
@@ -1,22 +1,26 @@
 from PIL import Image, ImageEnhance, ImageFilter
 import os
 
-path = "./imgs" # folder for unedited images
-pathOut = "./editedImgs" # folder for edited images
+path = "./imageProcessor/imgs" # folder for unedited images
+pathOut = "./imageProcessor/editedImgs" # folder for edited images
 
 for filename in os.listdir(path):
-    img = Image.open(f"{path}/{filename}")
+    
+    if filename == ".DS_Store":
+        continue
+    else:
+        img = Image.open(f"{path}/{filename}")
 
-    # sharpening, BW
-    edit = img.filter(ImageFilter.SHARPEN).convert('L').rotate(-90)
+        # sharpening, BW
+        edit = img.filter(ImageFilter.SHARPEN).convert('L')
 
-    # contrast
-    factor = 1.5
-    enhancer = ImageEnhance.Contrast(edit)
-    edit = enhancer.enhance(factor)
+        # contrast
+        factor = 1.5
+        enhancer = ImageEnhance.Contrast(edit)
+        edit = enhancer.enhance(factor)
 
-    # ADD MORE EDITS FROM DOCUMENTATION https://pillow.readthedocs.io/en/stable/
+        # ADD MORE EDITS FROM DOCUMENTATION https://pillow.readthedocs.io/en/stable/
 
-    clean_name = os.path.splitext(filename)[0]
+        clean_name = os.path.splitext(filename)[0]
 
-    edit.save(f'.{pathOut}/{clean_name}_edited.jpg')
+        edit.save(f'{pathOut}/{clean_name}_edited.jpg')
